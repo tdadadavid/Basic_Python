368915478312438331fff0ad02d46ac9d1b56665
tdadadavid <davidtofunmidada@gamil.com>
2021-05-20 13:38:22 +0100

    Both conditinal function(structure) give the same output , go with second method for a clean code

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-
-
 temperature = 30
 
 if temperature > 50:
     print("Really Hot!")
     print("Have a cold bath")
-elif temperature <= 30:
-    print("Temp is cool")
 else:
     print("The temperature is okay!")
+
+# Using the tenary oprator to write
+# the same logic we have
+
+message = "Really hot " and "Have a cold bath" if temperature > 50 else "The temperature is okay!"
+print(message)
