371e359a7ec9ec755f9b4c13ae7b00ed87abc2e1
tdadadavid <davidtofunmidada@gamil.com>
2021-05-20 16:12:06 +0100

    This my first function in python

--- app.py
+++ app.py
@@ -1,26 +1,8 @@
-# Practicing logical operaors
-# and , or , not
-
-high_income = False
-good_credit = True
-student = False
-
-if high_income and good_credit:
-    print("Eligible")
-else:
-    print("Not Eligible")
-
-# Output = not Eligble
+def greet(first_name, second_name):
+    print(f"Hello {first_name} {second_name}")
+    print("Welcome Aboard")
+    print(" ")
 
 
-if high_income or good_credit:
-    print("Eligible")
-else:
-    print("Not Eligible")
-
-# output  = Eligible
-
-if high_income or good_credit and not student:
-    print("Eligible")
-else:
-    print("Not Eligible")
+greet("David", "Dada")
+greet("King", "Tofu")
