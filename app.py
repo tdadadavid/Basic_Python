52c1882c40c6229d5cea96e1a72dc2e0d0eda0d6
tdadadavid <davidtofunmidada@gamil.com>
2021-05-20 11:18:08 +0100

    This is a very basic syntax in conditional statements, always remember the colon (:) after every condition

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-course = "                Python Programming                                   "
-# print(course.upper())
-# print(course.lower())
-# print(course.title())
-# print(course.rstrip())
-# print(course.lstrip())
-# print(course.find("P"))
-# print(course.replace("p", "J"))
-print("Kilode" not in course)
-print("Pyt" in course)
+
+
+temperature = 30
+
+if temperature > 50:
+    print("Really Hot!")
+    print("Have a cold bath")
+elif temperature <= 30:
+    print("Temp is cool")
+else:
+    print("The temperature is okay!")
