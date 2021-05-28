362ac15050f5502cf11f60e20baeea899565d2e5
tdadadavid <davidtofunmidada@gamil.com>
2021-05-28 19:27:59 +0100

    Learning about magic methods is really cool , try some out

--- Classes.py
+++ Classes.py
@@ -11,16 +11,16 @@ class Point:
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return f"({self.x} , {self.y})"
+
+    @classmethod
+    def zero(cls):
+        return cls(0, 0)
+
     def draw(self):
         print(f"Drawning points ({self.x} , {self.y})")
 
 
 point = Point(1, 2)
-point.default_color = "Red"
-print(point.default_color)
-print(Point.default_color)
-Point.draw(point)
-
-
-another_point = Point(4, 5)
-print("Another Point", another_point.default_color)
+print(str(point))
