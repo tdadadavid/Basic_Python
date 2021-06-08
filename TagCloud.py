0352b6f5c4ade538d7d9b80cad68d9b116d8c8de
tdadadavid <davidtofunmidada@gamil.com>
2021-06-08 13:58:34 +0100

    Multiple Inheritance in python

--- TagCloud.py
+++ TagCloud.py
@@ -1,19 +1,49 @@
-class Animal:
-    def eat(self):
-        print("eat")
+# class Animal:
+#     def __init__(self):
+#         print("Animal Constructor")
+#         self.age = 1
+
+#     def eat(self):
+#         print("eat")
 
 
-class Mammal(Animal):
-    def walk(self):
-        print("walk")
+# class Mammal(Animal):
+#     def __init__(self):
+#         super().__init__()
+#         print("Mammal Constructor")
+#         self.weight = 2
+
+#     def walk(self):
+#         print("walk")
 
 
-class Aves(Animal):
-    def fly(self):
-        print("fly")
+# class Aves(Animal):
+#     def fly(self):
+#         print("fly")
 
 
-m = Mammal()
-a = Aves()
-print(m.walk())
-print(a.eat())
+# m = Mammal()
+# a = Aves()
+# print(m.weight)
+# print(m.age)
+
+
+# Multiple inheritance
+
+
+class Swimmer:
+    def swim(self):
+        print("Swim")
+
+
+class Flyer:
+    def swim(self):
+        print("Fly")
+
+
+class Duck(Swimmer, Flyer):
+    pass
+
+
+duck = Duck()
+duck.swim()
