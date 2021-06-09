c7e5e1e3ae5914e0594ffcfc76a31e64cbec6f16
tdadadavid <davidtofunmidada@gamil.com>
2021-06-09 04:44:35 +0100

    Using namedtuple instead of magicMethods

--- Classes.py
+++ Classes.py
@@ -1,36 +1,23 @@
-# Class: is the blueprint of an object
-# Object: an instance of a class
+from collections import namedtuple
 
-from ast import Str
+# instead of using this tideous format below
+# we can use a namedtuple from the collections
+# class...
+# Note that namedtuple is immutable
 
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
 
-class Point:
-    default_color = "Pink"
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    @classmethod
-    def zero(cls):
-        return cls(0, 0)
-
-    def draw(self):
-        print(f"Drawning points ({self.x} , {self.y})")
-
-    def __str__(self):
-        return f"({self.x} , {self.y})"
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __gt__(self, other):
-        return self.x > other.x and self.y > other.y
-
-    def __add__(self, other):
-        return (self.x + other.x, self.y + other.y)
+#     def __eq__(self, o: object) :
+#         return self.x == o.x and self.y == o.y
 
 
-point = Point(1, 2)
-point2 = Point(1, -2)
-print(point + point2)
+Point = namedtuple("Point", ["x", "y"])
+
+# code is not noisy...Hurrray
+
+point = Point(x=1, y=2)
+point2 = Point(x=1, y=2)
+print(point == point2)
